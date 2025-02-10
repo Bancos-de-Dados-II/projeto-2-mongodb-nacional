62edd31ae8fb73d22e97ab7a72bde295884541d3
AlvesLariany <info.larianyalves@gmail.com>
2025-02-09 21:32:41 -0300

    feat: Tela de contato e navegação

--- frontEnd/src/routes/App.jsx
+++ frontEnd/src/routes/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
+import Contato from "../pages/Contato"
 
 //rotas para paginação entre telas
 export default function App() {
@@ -7,7 +8,7 @@ export default function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/contato" element={""} />
+        <Route path="/contato" element={<Contato />} />
         <Route path="/login" element={""} />
         <Route path="/cadastro" element={""} />
       </Routes>
