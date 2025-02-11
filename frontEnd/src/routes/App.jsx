f2fec8a5b72655bd3cf1f929cb7244ddd6169c33
Daniel de Morais <fariasniel05@gmail.com>
2025-02-11 19:12:25 -0300

    Adicionando telas de login e cadastro

--- frontEnd/src/routes/App.jsx
+++ frontEnd/src/routes/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
+import { Login } from "../pages/Login";
+import { Register } from "../pages/Register";
 
 //rotas para paginação entre telas
 export default function App() {
@@ -8,8 +10,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contato" element={""} />
-        <Route path="/login" element={""} />
-        <Route path="/cadastro" element={""} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/cadastro" element={<Register/>} />
       </Routes>
     </Router>
   );
